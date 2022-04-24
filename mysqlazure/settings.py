ae9337b0e8c29cfa921319ff22037ed67fcf0ac8
unknown <munmun.holyfamily@gmail.com>
2022-04-24 11:55:52 +0530

    second

--- mysqlazure/settings.py
+++ mysqlazure/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z88$b$%r#=@@8+1s1v5ief8(b4s3ffp)g@r65x#1pi!&3u3xp*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
